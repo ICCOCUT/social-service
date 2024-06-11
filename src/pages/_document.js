88793b4bac0b987f22b7aa4c180c1db2414c4e6a
Luis Fernando Ramírez Rivas <luis.ramirez4415@alumnos.udg.mx>
2024-06-11 17:11:53 -0600

    Add footer and header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Header from "../pages/components/header"
+import Footer from "../pages/components/Footer"
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Header />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>
