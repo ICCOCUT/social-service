8b25d04b63fdb06cb14c4fe40b740738a948225c
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2024-06-19 16:14:01 -0600

    pepe

--- src/pages/CostCalculator.jsx
+++ src/pages/CostCalculator.jsx
@@ -52,16 +52,16 @@ const CostoPorHora = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-900">
-            <div className="bg-gray-300 text-gray-700 p-8 rounded-lg shadow-md w-full max-w-md">
+            <div className="bg-gray-600 text-gray-200 p-8 rounded-lg shadow-md w-full max-w-md">
                 <h2 className="text-2xl font-bold mb-6 text-center">Calculadora de Costo por Hora</h2>
                 {!gastosFijos && <ExpenseFormPopup onSubmit={handleGastosFijosSubmit} />}
                 <div className="mt-4">
                     {gastosFijos !== null && (
                         <div>
-                            <h3 className="text-lg font-bold mb-4 text-center text-black">Gastos Fijos</h3>
-                            <p className="text-center text-black">Total de gastos fijos: ${gastosFijos.toFixed(2)}</p>
-                            <h3 className="text-lg font-bold mb-4 text-center text-black">Costo por Hora (sin porcentaje)</h3>
-                            <p className="text-center text-black">
+                            <h3 className="text-lg font-bold mb-4 text-center">Gastos Fijos</h3>
+                            <p className="text-center">Total de gastos fijos: ${gastosFijos.toFixed(2)}</p>
+                            <h3 className="text-lg font-bold mb-4 text-center">Costo por Hora (sin porcentaje)</h3>
+                            <p className="text-center">
                                 ${(gastosFijos / 208).toFixed(2)}
                             </p>
                             <div className="flex justify-center mt-4">
@@ -84,7 +84,7 @@ const CostoPorHora = () => {
                     )}
                     {costoPorHora.length > 0 && (
                         <div className="mt-6">
-                            <h3 className="text-lg font-bold mb-4 text-center text-black">Tu tiempo vale</h3>
+                            <h3 className="text-lg font-bold mb-4 text-center">Tu tiempo vale</h3>
                             <ul className="space-y-2">
                                 {costoPorHora.map((result, index) => (
                                     <li key={index} className="bg-blue-500 text-white p-3 rounded-md shadow-sm">
